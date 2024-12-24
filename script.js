d64bd0f46e96db9bf841b535267ac95e443bacb1
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2024-12-24 22:30:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,22 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	
+ if (arr.length <= 1) return 0; 
+    let totalCost = 0;
+    arr.sort((a, b) => a - b);
+
+    arr.forEach(() => {
+        if (arr.length > 1) {
+            const rope1 = arr.shift();
+            const rope2 = arr.shift();
+            totalCost += cost;
+            arr.push(cost);
+            arr.sort((a, b) => a - b);
+        }
+    });
+
+    return totalCost;
 }
 
 module.exports=mincost;
