43ca82f96d433ebdd4571f89a707cae82f609bd0
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2024-12-24 22:38:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function mincost(arr)
         if (arr.length > 1) {
             const rope1 = arr.shift();
             const rope2 = arr.shift();
+			const cost = rope1 + rope2;
             totalCost += cost;
             arr.push(cost);
             arr.sort((a, b) => a - b);
